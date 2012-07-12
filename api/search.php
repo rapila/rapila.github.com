69619da6c1df40762ed3da8f4ed816f4e659969c
root <root@rapi.vzone.ch>
2012-07-12 03:08:52 +0200

    Updated API

--- api/search.php
+++ api/search.php
@@ -374,7 +374,7 @@ main();
 
 
 ?>
-<hr size="1"><address style="align: right;"><small>Generated on Wed Jul 11 03:07:14 2012 for rapila by&nbsp;
+<hr size="1"><address style="align: right;"><small>Generated on Thu Jul 12 03:08:46 2012 for rapila by&nbsp;
 <a href="http://www.doxygen.org/index.html">
 <img src="doxygen.png" alt="doxygen" align="middle" border="0"></a> 1.4.7 </small></address>
 </body>
