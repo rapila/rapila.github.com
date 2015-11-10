939ac77a55e34d40eaae8727b2ddac7c946d40d0
Apache <apache@vps.foundit.ch>
2015-11-10 09:43:27 +0100

    Update API

--- api/search/pages_3.js
+++ api/search/pages_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['rapila_3a_20another_20php_20cms',['rapila: another PHP CMS',['../index.html',1,'']]],
-  ['rapila',['Rapila',['../md_cms-full_base_Readme.html',1,'']]]
+  ['rapila_20_21_5bbuild_20status_5d_28https_3a_2f_2fapi_2etravis_2dci_2eorg_2frapila_2fcms_2dfull_2esvg_29',['Rapila ![Build status](https://api.travis-ci.org/rapila/cms-full.svg)',['../md_cms-full_base_Readme.html',1,'']]]
 ];
